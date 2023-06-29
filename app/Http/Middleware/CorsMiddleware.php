9322da2698aeb6bebe6bb1fca5e9ebca2b6fd487
Joel Onwuanaku <joelebukatobi@pop-os.localdomain>
2023-06-29 10:27:28 +0100

    Fixed some issues

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,26 +13,10 @@ class CorsMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
 
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        $allowedOrigins = [
-            // Add your allowed origins here
-            // Example: Allow requests from a local development server
-            'http://localhost:5173',
-
-            // Example: Allow requests from your production domain
-            // 'https://buzz.stack',  
-        ];
-
-        $origin = $request->header('Origin');
-
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
-        }
-
-        return $next($request);
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
